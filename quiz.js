cbe61fb7c3df37d83835049ef7018c054071e615
Emily Chai <echai93@gmail.com>
2017-09-22 12:35:51 +0100

    WIP to do logic to calculate number of times each dimension chosne

---
+++ quiz.js
@@ -0,0 +1,121 @@
+function buildAssessment() {}
+  const output = [];
+
+  questions.forEach(
+    (currentQuestion, questionNumber) => {
+      const answers = [];
+
+      for(letter in currentQuestion.answers) {
+
+        answers.push(
+          <label>
+          <input type='radio'name='question${questionNumber}'
+          value='${letter}'>
+          ${letter} :
+          ${currentQuestion.answers[letter]}
+          </label>
+        );
+      }
+
+      output.push(
+        <div class='question'> ${currentQuestion.question} </div>
+        <div class='answers'> ${answers.join('')} </div>
+      );
+    }
+  };
+
+  quizContainer.innerHTML = output.join('');
+
+function showResults() {
+
+  const answerContainers = assessmentContainer.querySelectorAll('.answers');
+
+  questions.forEach( (currentQuestion, questionNumber) => {
+    // keep track of number of times each dimension is chosen
+    let numAdaptive = 0;
+    let numIntegrity = 0;
+    let numCollaborative = 0;
+    let numResult = 0;
+    let numCustomer = 0;
+    let numDetail = 0;
+
+    const answerContainer = answerContainers[questionNumber];
+    const selector = 'input[name=question'+questionNumber+']:checked';
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    if(userAnswer===currentQuestion.dimensionA){
+
+    }
+  });
+
+  resultsContainer.innerHTML = 'Adaptive: ' + numAdaptive + 'Integrity: ' + 'numIntegrity'
+  + 'Collaborative: ' + numCollaborative + 'Result: ' + numResult + 'Customer: ' + numCustomer
+  + 'Detail: ' + numDetail
+}
+
+// Display quiz
+buildAssessment();
+
+// On submit, show results
+submitButton.addEventListener('click', showResults);
+
+const assessmentContainer = document.getElementById('assessment');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+const questions = [
+  {
+    question: "Pick the option that describes you best:",
+    options: {
+      a: "I am innovative",
+      b: "I have a high sense of integrity"
+    },
+    dimensionA: "Adaptive",
+    dimensionB: "Integrity"
+  },
+  {
+    question: "Pick the option that describes you best:",
+    options: {
+      a: "I take risks",
+      b: "I am team oriented"
+    },
+    dimensionA: "Adaptive",
+    dimensionB: "Collaborative"
+  },
+  {
+    question: "Pick the option that describes you best:",
+    options: {
+      a: "I have high ethical standards",
+      b: "I am cooperative"
+    },
+    dimensionA: "Integrity",
+    dimensionB: "Collaborative"
+  },
+  {
+    question: "Pick the option that describes you best:",
+    options: {
+      a: "I am results oriented",
+      b: "I am customer oriented"
+    },
+    dimensionA: "Result",
+    dimensionB: "Customer"
+  },
+  {
+    question: "Pick the option that describes you best:",
+    options: {
+      a: "I have high expectations for performance",
+      b: "I pay attention to detail"
+    },
+    dimensionA: "Result",
+    dimensionB: "Detail"
+  },
+  {
+    question: "Pick the option that describes you best:",
+    options: {
+      a: "I listen to customers",
+      b: "I put emphasis on quality"
+    },
+    dimensionA: "Customer",
+    dimensionB: "Detail"
+  }
+]
