df3917e5c2aed977b32153aea4ff7768c488b81d
Emily Chai <echai93@gmail.com>
2017-09-24 15:51:25 +0100

    Function to show the slides

--- quiz.js
+++ quiz.js
@@ -65,7 +65,34 @@
     + 'Detail: ' + numDetail
   }
 
-  $(["data-dimension='adaptive']:selected").length
+  // $(["data-dimension='adaptive']:selected").length
+
+  // pagination
+  const previousButton = document.getElementById('previous');
+  const nextButton = document.getElementById('next');
+  const slides = document.querySelectorAll('.slide');
+  let currentSlide = 0;
+
+  function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0) {
+      previousButton.style.display = 'none';
+    }
+    else {
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide ==== slides.length-1) {
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else {
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
+  showSlide(0);
 
   const assessmentContainer = document.getElementById('assessment');
   const resultsContainer = document.getElementById('results');
