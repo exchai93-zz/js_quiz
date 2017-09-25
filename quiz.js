b08552bef51b445bf9d069acc56f18501982dd7c
Emily Chai <echai93@gmail.com>
2017-09-25 16:32:53 +0100

    Correct hash structure

--- quiz.js
+++ quiz.js
@@ -1,8 +1,109 @@
+'use strict';
 (function(){
+  const options = [
+    {
+      "Adaptive": [
+                  "I am innovative",
+                  "I take risks",
+                  "I am willing to experiment",
+                  "I am fast moving",
+                  "I am quick to take advantage of opportunities",
+                  "I don't like to be constrained by rules",
+                  "I am flexible",
+                  "I adjust easily to changing environments",
+                  "I welcome change",
+                  "I am a curious person"
+                ],
+      "Integrity" : [
+                  "I have a high sense of integrity",
+                  "I have high ethical standards",
+                  "I am honest",
+                  "I respect individuals",
+                  "I am fair",
+                  "I am trustworthy",
+                  "I always tell the truth",
+                  "I lead by example",
+                  "I am not easily offended",
+                  "I always try to do the right thing"
+                ],
+      "Collaborative" : [
+                  "I am team oriented",
+                  "I am cooperative",
+                  "I am supportive",
+                  "I work in collaboration with others",
+                  "I avoid conflicts",
+                  "I am always happy to share knowledge",
+                  "I am a good communicator",
+                  "I share responsibilities",
+                  "I like working together with others",
+                  "I am always available to help out a colleague in need"
+                ],
+      "Result" : [
+                "I am results oriented",
+                "I have high expectations for performance",
+                "I am achievement oriented",
+                "I reach my goals",
+                "I am ambitious",
+                "I can sometimes be aggresive",
+                "I set clear goals",
+                "I am good at identifying effective processes",
+                "I am action oriented",
+                "I focus on the outcome, rather than the process"
+              ],
+    "Customer" : [
+                "I am customer oriented",
+                "I listen to customers",
+                "I watch the markets carefully",
+                "I value our clients",
+                "I respond promptly to clients",
+                "I always put customer needs first",
+                "I work to satisfy our customers",
+                "I put the customer's requirements ahead of anything else",
+                "I highly value customer feedback",
+                "I base my decisions on what our customers need & want"
+              ],
+    "Detail" : [
+                "I pay attention to detail",
+                "I put emphasis on quality",
+                "I am precise",
+                "I focus on rules",
+                "I am careful",
+                "I see patterns in data",
+                "I am analytical ",
+                "I am good at understanding cause-and-effect relationships",
+                "I am accurate in my work",
+                "I notice mistakes quickly"
+              ],
+    }
+  ];
+
   function buildAssessment() {}
     // Store HTML output
     const output = [];
+
+    var numberOfQuestions = 30;
+
+    function randomDim(obj) {
+      var result;
+      var count = 0;
+      for (var prop in obj)
+        if (Math.random() < 1/++ count)
+          result = prop;
+        return result;
+    }
+
+    for (i=0; i < numberOfQuestions; i++) {
+      console.log("Question #" + i);
+      var randomDimension = options[randomDim(options)];
+      console.log(randomDim(options) + ": " + randomDimension[Math.floor((Math.random() * 2) + 1)]);
+      console.log(randomDim(options) + ": " + randomDimension[Math.floor((Math.random() * 2) + 1)]);
+      console.log("################");
+    }
     // For each question
+    // Pick to key values to generate each question - pop 2 values off the options hash
+
+    // iterations based on a number
+
     questions.forEach(
       (currentQuestion, questionNumber) => {
         // Store the list of answer choices
@@ -18,13 +119,15 @@
             data-dimension='${dimension}'>
             ${letter} :
             ${currentQuestion.answers[letter]}
-            ${currentQuestion.dimension[dimension]}
             </label>`
           );
         }
+
+        ${currentQuestion.dimension[dimension]}
         // pass the dimenion to the value of a radio button - in the name or in a data attribute i.e. data-dimension
         // $(["data-dimension='adaptive']:selected").length
 
+        // Add question and answers to the output
         output.push(
           `<div class='slide'>
           <div class='question'>Pick the option that describes you best:</div>
@@ -32,7 +135,7 @@
           </div>`
         );
       });
-      quizContainer.innerHTML = output.join('');
+      assessmentContainer.innerHTML = output.join('');
     }
 
 
@@ -110,80 +213,7 @@
   const assessmentContainer = document.getElementById('assessment');
   const resultsContainer = document.getElementById('results');
   const submitButton = document.getElementById('submit');
-  const options = [
-    {
-      Adaptive: {["I am innovative",
-                  "I take risks",
-                  "I am willing to experiment",
-                  "I am fast moving",
-                  "I am quick to take advantage of opportunities",
-                  "I don't like to be constrained by rules",
-                  "I am flexible",
-                  "I adjust easily to changing environments",
-                  "I welcome change",
-                  "I am a curious person"]}
-    },
-    {
-      Integrity: {["I have a high sense of integrity",
-      "I have high ethical standards",
-      "I am honest",
-      "I respect individuals",
-      "I am fair",
-      "I am trustworthy",
-      "I always tell the truth",
-      "I lead by example",
-      "I am not easily offended",
-      "I always try to do the right thing"]}
-    },
-    {
-      Collaborative: {[ "I am team oriented",
-      "I am cooperative",
-      "I am supportive",
-      "I work in collaboration with others",
-      "I avoid conflicts",
-      "I am always happy to share knowledge",
-      "I am a good communicator",
-      "I share responsibilities",
-      "I like working together with others",
-      "I am always available to help out a colleague in need"]}
-    },
-    {
-      Result: {["I am results oriented",
-      "I have high expectations for performance",
-      "I am achievement oriented",
-      "I reach my goals",
-      "I am ambitious",
-      "I can sometimes be aggresive",
-      "I set clear goals",
-      "I am good at identifying effective processes",
-      "I am action oriented",
-      "I focus on the outcome, rather than the process"]}
-    },
-    {
-    Customer: {[ "I am customer oriented",
-      "I listen to customers",
-      "I watch the markets carefully",
-      "I value our clients",
-      "I respond promptly to clients",
-      "I always put customer needs first",
-      "I work to satisfy our customers",
-      "I put the customer's requirements ahead of anything else",
-      "I highly value customer feedback",
-      "I base my decisions on what our customers need & want"]}
-    },
-    {
-    Detail: {["I pay attention to detail",
-      "I put emphasis on quality",
-      "I am precise",
-      "I focus on rules",
-      "I am careful",
-      "I see patterns in data",
-      "I am analytical ",
-      "I am good at understanding cause-and-effect relationships",
-      "I am accurate in my work",
-      "I notice mistakes quickly"]}
-    }
-  ];
+
   // Display quiz
   buildAssessment();
 
